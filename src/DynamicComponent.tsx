26bd6a4a49d0851179df78e93a1024f86d28d317
fortune-max <31856097+fortune-max@users.noreply.github.com>
2023-02-17 05:24:05 +0100

    update files

--- src/DynamicComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DynamicComponent = () => {
-    return (
-        <div data-testid="dynamic-component">This is a Dynamic Component</div>
-    );
-};
-
-export default DynamicComponent;
