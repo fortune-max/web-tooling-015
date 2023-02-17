0ad067549ba2bd5d752739261b5dec56d642bf97
fortune-max <fortmax120@gmail.com>
2023-02-17 06:42:39 +0100

    update CodeRenderer styles

--- src/CodeRenderer.tsx
+++ src/CodeRenderer.tsx
@@ -3,13 +3,27 @@ import Highlight, { defaultProps } from "prism-react-renderer";
 
 const CodeRenderer = ({ codeSrc }: { codeSrc: string }) => {
     const exampleCode = `
-(function someDemo() {
-  var test = "Hello World!";
-  console.log(test);
-})();
+import React from "react";
 
-return () => <App />;
+const DynamicComponent = () => {
+    return (
+        <div data-testid="dynamic-component">This is a Dynamic Component</div>
+    );
+};
+
+export default DynamicComponent;
 `;
+
+    const styles = {
+        color: "rgb(154, 134, 253)",
+        width: "750px",
+        height: "400px",
+        overflow: "scroll",
+        padding: "20px",
+        backgroundColor: "midnightblue",
+        border: "5px solid black",
+        borderRadius: "10px",
+    };
 
     const Content = (
         <Highlight
@@ -18,7 +32,7 @@ return () => <App />;
             language="jsx"
         >
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={style}>
+                <pre className={className} style={styles}>
                     {tokens.map((line, i) => (
                         // eslint-disable-next-line react/jsx-key
                         <div {...getLineProps({ line, key: i })}>
@@ -33,13 +47,7 @@ return () => <App />;
         </Highlight>
     );
 
-    return (
-        <div>
-            <h1>CodeRenderer</h1>
-            <p>{codeSrc}</p>
-            {Content}
-        </div>
-    );
+    return Content;
 };
 
 export default CodeRenderer;
