ddd811704a68a052bb36e66d11b87c5a7bd5eda8
fortune-max <fortmax120@gmail.com>
2023-02-20 22:25:35 +0100

    updates

--- src/DynamicComponent.tsx
+++ src/DynamicComponent.tsx
@@ -1,38 +1,9 @@
-/* eslint-disable prettier/prettier */
-import { useState } from "react";
 import React from "react";
-function DynamicComponent() {
-    const [counter, setCounter] = useState(0);
-    const increase = () => {
-        setCounter((count) => count + 1);
-    };
-    const decrease = () => {
-        setCounter((count) => count - 1);
-    };
-    const reset = () => {
-        setCounter(0);
-    };
+
+const DynamicComponent = () => {
     return (
-        <div className="dynamic-component">
-            {" "}
-            <h1>React Counter</h1>{" "}
-            <span className="counter__output">{counter}</span>{" "}
-            <div className="btn__container">
-                {" "}
-                <button className="control__btn" onClick={increase}>
-                    {" "}
-                    +{" "}
-                </button>{" "}
-                <button className="control__btn" onClick={decrease}>
-                    {" "}
-                    -{" "}
-                </button>{" "}
-                <button className="reset" onClick={reset}>
-                    {" "}
-                    Reset{" "}
-                </button>{" "}
-            </div>{" "}
-        </div>
+        <div data-testid="dynamic-component">This is a Dynamic Component</div>
     );
-}
+};
+
 export default DynamicComponent;
