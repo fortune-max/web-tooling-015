95c9e1e7f3ad0b601758eda3b88e988de819826f
fortune-max <31856097+fortune-max@users.noreply.github.com>
2023-02-21 16:08:39 +0100

    update files

--- src/DynamicComponent.tsx
+++ src/DynamicComponent.tsx
@@ -1,38 +1,87 @@
 /* eslint-disable prettier/prettier */
-import { useState } from "react";
 import React from "react";
-function DynamicComponent() {
-    const [counter, setCounter] = useState(0);
-    const increase = () => {
-        setCounter((count) => count + 1);
-    };
-    const decrease = () => {
-        setCounter((count) => count - 1);
-    };
-    const reset = () => {
-        setCounter(0);
-    };
+type todo = { text: string, isCompleted: boolean };
+type TodoItemProps = {
+    todo: todo,
+    index: number,
+    completeTodo: (index: number) => void,
+    removeTodo: (index: number) => void,
+};
+const TodoItem = ({ todo, index, completeTodo, removeTodo }: TodoItemProps) => {
     return (
-        <div data-testid="dynamic-component">
+        <div
+            className="todo-item"
+            data-testid="todo-item"
+            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        >
             {" "}
-            <h1>React Counter</h1>{" "}
-            <span className="counter__output">{counter}</span>{" "}
-            <div className="btn__container">
+            {todo.text}{" "}
+            <div>
                 {" "}
-                <button className="control__btn" onClick={increase}>
-                    {" "}
-                    +{" "}
+                <button onClick={() => completeTodo(index)}>
+                    Complete
                 </button>{" "}
-                <button className="control__btn" onClick={decrease}>
-                    {" "}
-                    -{" "}
-                </button>{" "}
-                <button className="reset" onClick={reset}>
-                    {" "}
-                    Reset{" "}
-                </button>{" "}
+                <button onClick={() => removeTodo(index)}>x</button>{" "}
             </div>{" "}
         </div>
     );
-}
+};
+type TodoFormProps = { addTodo: (text: string) => void };
+const TodoForm = ({ addTodo }: TodoFormProps) => {
+    const [value, setValue] = React.useState("");
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            {" "}
+            <input
+                type="text"
+                className="input"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />{" "}
+            <button type="submit">Save Todo</button>{" "}
+        </form>
+    );
+};
+const DynamicComponent = () => {
+    const [todos, setTodos]: [todo[], any] = React.useState([]);
+    const addTodo = (text: string) => {
+        const newTodos: todo[] = [...todos, { text, isCompleted: false }];
+        setTodos(newTodos);
+    };
+    const completeTodo = (index: number) => {
+        const newTodos: todo[] = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
+    };
+    const removeTodo = (index: number) => {
+        const newTodos: todo[] = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+    return (
+        <div className="todo" data-testid="dynamic-component">
+            {" "}
+            <h1>Todo List</h1>{" "}
+            <div data-testid="todo-list">
+                {" "}
+                {todos.map((todo, index) => (
+                    <TodoItem
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}{" "}
+            </div>{" "}
+            <TodoForm addTodo={addTodo} />{" "}
+        </div>
+    );
+};
 export default DynamicComponent;
